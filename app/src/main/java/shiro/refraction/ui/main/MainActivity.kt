92cb092c1511375cd813ace3dbdbe46d66be2376
Isla <isla@refraction.app>
2026-05-17 04:14:47 -0300

    Add missing WebStorage import in MainActivity

--- app/src/main/java/shiro/refraction/ui/main/MainActivity.kt
+++ app/src/main/java/shiro/refraction/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebSettings
+import android.webkit.WebStorage
 import android.webkit.WebView
 import android.widget.Toast
 import androidx.activity.viewModels
