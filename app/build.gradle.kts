e69b7de81a2867e9a5712b397e4a793394049141
Isla <isla@refraction.app>
2026-05-18 01:14:45 -0300

    Bump v1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "shiro.refraction"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.0"
+        versionCode = 8
+        versionName = "1.4.0"
     }
 
     signingConfigs {
